93416b1109dd7167cb9c718fbe53c96937c8dd92
Zisan Hasan Akash <97005736+zisanbd@users.noreply.github.com>
2026-01-12 11:41:22 +0600

    Update CalculatorServlet.java

--- CalculatorServlet.java
+++ CalculatorServlet.java
@@ -1,11 +1,15 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/calculate")
 public class CalculatorServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
 
         double num1 = Double.parseDouble(request.getParameter("num1"));
         double num2 = Double.parseDouble(request.getParameter("num2"));
@@ -13,17 +17,17 @@ public class CalculatorServlet extends HttpServlet {
 
         double result = 0;
 
-        switch (op) {
-            case "add": result = num1 + num2; break;
-            case "sub": result = num1 - num2; break;
-            case "mul": result = num1 * num2; break;
-            case "div": result = num1 / num2; break;
-        }
+        if (op.equals("add")) result = num1 + num2;
+        else if (op.equals("sub")) result = num1 - num2;
+        else if (op.equals("mul")) result = num1 * num2;
+        else if (op.equals("div")) result = num1 / num2;
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
+        out.println("<html><body>");
         out.println("<h2>Result: " + result + "</h2>");
         out.println("<a href='index.html'>Back</a>");
+        out.println("</body></html>");
     }
 }
